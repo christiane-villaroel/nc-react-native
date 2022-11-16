962be2d8d70d9a3879bdf9c6b3320796ede900df
Christiane Villaroel <christianevillaroel@gmail.com>
2022-11-16 09:24:56 -0500

    Animations

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,13 @@
-import * as React from "react";
-import { ScrollView,Text,View } from "react-native";
-import { Card } from "react-native-elements";
-import { useSelector } from "react-redux";
-import { baseUrl } from "../shared/baseUrl";
-import Loading from "../components/LoadingComponents";
+import { useEffect, useRef } from 'react';
+import { Text, View, Animated } from 'react-native';
+import { Card } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponents';
 
-
-function FeaturedItem(props) {
+const FeaturedItem = (props) => {
     const { item } = props;
+
     if (props.isLoading) {
         return <Loading />;
     }
@@ -22,7 +22,7 @@ function FeaturedItem(props) {
         return (
             <Card containerStyle={{ padding: 0 }}>
                 <Card.Image source={{ uri: baseUrl + item.image }}>
-                    <View style={{ justifyContent: "center", flex: 1 }}>
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text
                             style={{
                                 color: 'white',
@@ -30,47 +30,57 @@ function FeaturedItem(props) {
                                 fontSize: 20
                             }}
                         >
-
+                            {item.name}
                         </Text>
                     </View>
                 </Card.Image>
-                <Text style={{ margin: 20 }}>
-                    {item.description}
-                </Text>
+                <Text style={{ margin: 20 }}>{item.description}</Text>
             </Card>
         );
     }
     return <View />;
-}
+};
 
 const HomeScreen = () => {
-    const campsites = useSelector((state)=> state.campsites);
-    const promotions = useSelector((state)=> state.promotions);
-    const partners = useSelector((state)=> state.partners)
+    const campsites = useSelector((state) => state.campsites);
+    const promotions = useSelector((state) => state.promotions);
+    const partners = useSelector((state) => state.partners);
+    const scaleValue = useRef(new Animated.Value(0)).current;
+    const scaleAnimation = Animated.timing(scaleValue, {
+        toValue: 1,
+        duration: 1500,
+        useNativeDriver: true
+    });
 
-    const featCampsite = campsites.campsitesArray.find((item)=> item.featured);
-    const featPromotion = promotions.promotionsArray.find((item)=> item.featured);
+    const featCampsite = campsites.campsitesArray.find((item) => item.featured);
+    const featPromotion = promotions.promotionsArray.find(
+        (item) => item.featured
+    );
     const featPartner = partners.partnersArray.find((item) => item.featured);
 
+    useEffect(() => {
+        scaleAnimation.start();
+    }, []);
+
     return (
-        <ScrollView>
-            <FeaturedItem 
+        <Animated.ScrollView style={{ transform: [{ scale: scaleValue }] }}>
+            <FeaturedItem
                 item={featCampsite}
                 isLoading={campsites.isLoading}
                 errMess={campsites.errMess}
             />
             <FeaturedItem
-            item={featPromotion}
-            isLoading={promotions.isLoading}
-            errMess={promotions.errMess}
+                item={featPromotion}
+                isLoading={promotions.isLoading}
+                errMess={promotions.errMess}
             />
             <FeaturedItem
-            item={featPartner}
-            isLoading={partners.isLoading}
-            errMess={partners.errMess}
+                item={featPartner}
+                isLoading={partners.isLoading}
+                errMess={partners.errMess}
             />
-        </ScrollView>
-    )
-}
+        </Animated.ScrollView>
+    );
+};
 
 export default HomeScreen;
